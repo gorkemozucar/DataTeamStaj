10e94253930f671f313fe3cd4fdeaa3889cea59f
gorkemozucar <61429282+gorkemozucar@users.noreply.github.com>
2021-01-28 16:55:01 +0300

    NodeJs1

---
+++ Node Js DENEMELER/node js basics/global.js
@@ -0,0 +1,17 @@
+// console.log(global);
+
+// setTimeout(() => {             // belirtilen süre sonrası mesajı gösterir
+//     console.log('in the timeout');
+//     clearInterval(int);
+    
+// }, 3000);
+
+// const int = setInterval (() => {  // döngü halinde belirtilen saniyede mesajı gösterir
+//     console.log('in the interval');
+// }, 1000);
+
+//console.log(__dirname); //dosyanın bulunduğu adresi gösterir
+// console.log(_filename);
+
+// console.log(document.querySelector); // sorgu dökümanını gösterir
+
