c4963c7444803a680aa7b2f717a09d50c9f36232
gorkemozucar <61429282+gorkemozucar@users.noreply.github.com>
2021-02-08 02:22:56 +0300

    Express
    Express member app

---
+++ Express/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name:'John Doe',
+        email:'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name:'Nazan Top',
+        email:'nazantop@gmail.com',
+        status:'inactive'
+    },
+    {
+        id:3,
+        name:'Gorkem Ozucar',
+        email:'gorkemozucar@gmail.com',
+        status:'active'
+    }
+];
+
+module.exports = members;
