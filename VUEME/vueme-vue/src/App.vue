68d314f84cb232aa4c5bdf17596677bd1a8c1586
gorkemozucar <61429282+gorkemozucar@users.noreply.github.com>
2021-02-15 02:17:34 +0300

    VueME

---
+++ VUEME/vueme-vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Postcomponent/>
+  </div>
+</template>
+
+<script>
+import Postcomponent from './components/Postcomponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Postcomponent
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
